2854fa2c16075e373ed722d2a670dd8f89a55bb0
Michael Cruz <mecruz@bu.edu>
2019-07-28 01:16:51 -0400

    batch size 36

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
 flags.DEFINE_float('lambda_val', 0.5, 'down weight of the loss for absent digit classes')
 
 # for training
-flags.DEFINE_integer('batch_size', 64, 'batch size') # changed from 128 to 1
+flags.DEFINE_integer('batch_size', 36, 'batch size') # changed from 128 to 1
 flags.DEFINE_integer('epoch', 25, 'epoch') # changed from 50 to 1 
 flags.DEFINE_integer('iter_routing', 3, 'number of iterations in routing algorithm')
 flags.DEFINE_boolean('mask_with_y', True, 'use the true label to mask out target capsule or not')
@@ -44,7 +44,7 @@ flags.DEFINE_string('results', 'results', 'path for saving results')
 #   distributed setting    #
 ############################
 flags.DEFINE_integer('num_gpu', 1, 'number of gpus for distributed training')
-flags.DEFINE_integer('batch_size_per_gpu', 64, 'batch size on 1 gpu') # changed 128 to 1
+flags.DEFINE_integer('batch_size_per_gpu', 36, 'batch size on 1 gpu') # changed 128 to 1
 flags.DEFINE_integer('thread_per_gpu', 1, 'Number of preprocessing threads per tower.')
 
 cfg = tf.app.flags.FLAGS
