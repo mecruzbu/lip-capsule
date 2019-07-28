af4ae09565bf0c1dadc3d52b26c2aee6b79242a0
Michael Cruz <mecruz@bu.edu>
2019-07-28 01:07:58 -0400

    no message

---
+++ utils.py
@@ -0,0 +1,131 @@
+import os
+import scipy
+import numpy as np
+import tensorflow as tf
+
+
+def load_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'mnist')
+    if is_training:
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # trainX = loaded[16:].reshape((7800, 64, 64, 1)).astype(np.float32) # changed 60k to 7800 & changed 28 to 64
+        trainX = loaded[16:].reshape((15600, 160, 160, 1)).astype(np.float32) # changed 60k to 15.6k & changed 28 to 160
+
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # trainY = loaded[8:].reshape((7800)).astype(np.int32) # changed 60k to 7800
+        trainY = loaded[8:].reshape((15600)).astype(np.int32) # changed 60k to 7800
+
+        # trX = trainX[:7000] / 255. # changed 55k to 7k
+        # trY = trainY[:7000]        # changed 55k to 7k
+
+        # valX = trainX[7000:, ] / 255. # changed 55k to 7k
+        # valY = trainY[7000:] # changed 55k to 1k
+
+        # num_tr_batch = 7000 // batch_size # changed 55k to 7k
+        # num_val_batch = 800 // batch_size # changed 5k to 800
+
+# --start---------------------------------------- 5 x 5 -------------------------------------
+
+        trX = trainX[:14400] / 255. # changed 55000 to 14400
+        trY = trainY[:14400]        # changed 55000 to 14400
+
+        valX = trainX[14400:, ] / 255.  # changed 55000 to 14400
+        valY = trainY[14400:]  	# changed 55000 to 14400
+
+        num_tr_batch = 14400 // batch_size  # changed 55000 to 14400
+        num_val_batch = 1200 // batch_size  # changed 5000 to 1200
+
+# ---end------------------------------------------ 5 x 5 -------------------------------------
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    else:
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # teX = loaded[16:].reshape((1200, 64, 64, 1)).astype(np.float) # changed 10000 to 1200 & changed 28 to 64
+        teX = loaded[16:].reshape((2400, 160, 160, 1)).astype(np.float) # changed 10000 to 2400
+ 
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # teY = loaded[8:].reshape((1200)).astype(np.int32)
+        teY = loaded[8:].reshape((2400)).astype(np.int32) # changed 10000 to 2400
+
+
+        # num_te_batch = 1200 // batch_size
+        num_te_batch = 2400 // batch_size  # changed 10000 to 2400
+        
+        return teX / 255., teY, num_te_batch
+
+
+# removed fashion MNIST method
+
+def load_data(dataset, batch_size, is_training=True, one_hot=False):
+    if dataset == 'mnist':
+        return load_mnist(batch_size, is_training)
+    else:
+        raise Exception('Invalid dataset, please check the name of dataset:', dataset)
+
+
+def get_batch_data(dataset, batch_size, num_threads):
+    if dataset == 'mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_mnist(batch_size, is_training=True)
+   
+    data_queues = tf.train.slice_input_producer([trX, trY])
+    X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 64,
+                                  min_after_dequeue=batch_size * 32,
+                                  allow_smaller_final_batch=False)
+
+    return(X, Y)
+
+
+def save_images(imgs, size, path):
+    '''
+    Args:
+        imgs: [batch_size, image_height, image_width]
+        size: a list with tow int elements, [image_height, image_width]
+        path: the path to save images
+    '''
+    imgs = (imgs + 1.) / 2  # inverse_transform
+    return(scipy.misc.imsave(path, mergeImgs(imgs, size)))
+
+
+def mergeImgs(images, size):
+    h, w = images.shape[1], images.shape[2]
+    imgs = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        imgs[j * h:j * h + h, i * w:i * w + w, :] = image
+
+    return imgs
+
+
+# For version compatibility
+def reduce_sum(input_tensor, axis=None, keepdims=False):
+    try:
+        return tf.reduce_sum(input_tensor, axis=axis, keepdims=keepdims)
+    except:
+        return tf.reduce_sum(input_tensor, axis=axis, keep_dims=keepdims)
+
+
+# For version compatibility
+def softmax(logits, axis=None):
+    try:
+        return tf.nn.softmax(logits, axis=axis)
+    except:
+        return tf.nn.softmax(logits, dim=axis)
+
+
+def get_shape(inputs, name=None):
+    name = "shape" if name is None else name
+    with tf.name_scope(name):
+        static_shape = inputs.get_shape().as_list()
+        dynamic_shape = tf.shape(inputs)
+        shape = []
+        for i, dim in enumerate(static_shape):
+            dim = dim if dim is not None else dynamic_shape[i]
+            shape.append(dim)
+        return(shape)
